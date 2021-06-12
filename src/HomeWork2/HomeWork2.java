3691945dafcb3c96bd7983aeccacd0e08c6edb38
Olga Nelipa <olg.nelipa@gmail.com>
2021-06-12 22:51:49 +0300

    feat: added homework3, improved homework2

--- src/HomeWork2/HomeWork2.java
+++ src/HomeWork2/HomeWork2.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class HomeWork2 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Which task do you want?");
+        int task = in.nextInt();
 
-        homeTask(args);
+        if (task == 1) {
+            homeTask(args);
+        } else {
+            System.out.println("wrong task");
+        }
 
     }
-
     public static void homeTask(String[] args) {
         Scanner in = new Scanner(System.in);
-
 
         System.out.println("Write a year");
         int year = in.nextInt();
@@ -24,119 +29,61 @@ public class HomeWork2 {
                 "July", "August", "September",
                 "October", "November", "December"
         };
-        //String[] arrayOfMonth = new String[1];
 
-        // if the year is leap
-        // if(  year % 4 == 0 && year % 400 != 0 ) {leap} else {isn't leap}
-        if ( year % 4 == 0 ) {
-            if (year % 400 != 0) {
-                    //is leap
-                switch(month) {
-                    case 1:
-                        //january
-                        daysInMonth = 31;
-                        break;
-                    case 2:
-                        //february
-                        daysInMonth = 29;
-                        break;
-                    case 3:
-                        //march
-                        daysInMonth = 31;
-                        break;
-                    case 4:
-                        //april
-                        daysInMonth = 30;
-                        break;
-                    case 5:
-                        //may
-                        daysInMonth = 31;
-                        break;
-                    case 6:
-                        //june
-                        daysInMonth = 30;
-                        break;
-                    case 7:
-                        //july
-                        daysInMonth = 31;
-                        break;
-                    case 8:
-                        //august
-                        daysInMonth = 31;
-                        break;
-                    case 9:
-                        //september
-                        daysInMonth = 30;
-                        break;
-                    case 10:
-                        //october
-                        daysInMonth = 31;
-                        break;
-                    case 11:
-                        //november
-                        daysInMonth = 30;
-                        break;
-                    case 12:
-                        //december
-                        daysInMonth = 31;
-                        break;
-                    }
-                }
-        } else{
-            //isn't leap
-            switch(month) {
-                case 1:
-                    //january
-                    daysInMonth = 31;
-                    break;
-                case 2:
-                    //february
+        switch(month) {
+            case 1:
+                //january
+                daysInMonth = 31;
+                break;
+            case 2:
+                //february
+                if ( year % 4 == 0 && year % 400 != 0 )
+                    daysInMonth = 29;
+                else
                     daysInMonth = 28;
-                    break;
-                case 3:
-                    //march
-                    daysInMonth = 31;
-                    break;
-                case 4:
-                    //april
-                    daysInMonth = 30;
-                    break;
-                case 5:
-                    //may
-                    daysInMonth = 31;
-                    break;
-                case 6:
-                    //june
-                    daysInMonth = 30;
-                    break;
-                case 7:
-                    //july
-                    daysInMonth = 31;
-                    break;
-                case 8:
-                    //august
-                    daysInMonth = 31;
-                    break;
-                case 9:
-                    //september
-                    daysInMonth = 30;
-                    break;
-                case 10:
-                    //october
-                    daysInMonth = 31;
-                    break;
-                case 11:
-                    //november
-                    daysInMonth = 30;
-                    break;
-                case 12:
-                    //december
-                    daysInMonth = 31;
-                    break;
-            }
+                break;
+            case 3:
+                //march
+                daysInMonth = 31;
+                break;
+            case 4:
+                //april
+                daysInMonth = 30;
+                break;
+            case 5:
+                //may
+                daysInMonth = 31;
+                break;
+            case 6:
+                //june
+                daysInMonth = 30;
+                break;
+            case 7:
+                //july
+                daysInMonth = 31;
+                break;
+            case 8:
+                //august
+                daysInMonth = 31;
+                break;
+            case 9:
+                //september
+                daysInMonth = 30;
+                break;
+            case 10:
+                //october
+                daysInMonth = 31;
+                break;
+            case 11:
+                //november
+                daysInMonth = 30;
+                break;
+            case 12:
+                //december
+                daysInMonth = 31;
+                break;
         }
 
         System.out.println(year + " " + arrayOfMonth[month-1] + " has " + daysInMonth + " days.");
     }
-
 }
