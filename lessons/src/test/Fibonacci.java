7957ba71fd4b2000694caad3ae22897901b36f5b
Olga Nelipa <olg.nelipa@gmail.com>
2021-07-10 13:07:08 +0300

    feat: added task Fibonacci

--- lessons/src/test/Fibonacci.java
+++ lessons/src/test/Fibonacci.java
@@ -1,15 +1,22 @@
 package test;
-
+import java.util.*;
 import java.util.Scanner;
 
 public class Fibonacci {
     public void task4(){
-        for (int i = 0; i < 50; i++){
-            if(i == 0 || i == 1){
-                System.out.println(i);
-            } else if((i-1)+(i-2) == i){
-                System.out.println(i);
-            }
+        long item = 0;
+        Stack<Long> array = new Stack<Long>();
+        array.push(0L);
+        array.push(1L);
+        System.out.println(array.get(0));
+        System.out.println(array.get(1));
+        for (int i = 1; i < 100; i++){
+            item = array.get(i) + array.get(i-1);
+                array.push(item);
+
+
+            System.out.println(array.get(i+1));
+
         }
     }
 }
